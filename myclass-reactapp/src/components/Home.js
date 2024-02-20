f972382bb74e6a59357fa7e19a412f244023857e
muhayimanatheo <muhayitheo23@gmail.com>
2024-02-20 13:10:27 +0200

    in prograss

---
+++ myclass-reactapp/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function Home(){
+    return(
+        <div>
+            <h1>Home page</h1>
+            <a href='/services'>Services page</a>
+        </div>
+    )
+}
+
+export default Home;
---
+++ myclass-reactapp/src/components/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ServicesFunc() {
+    return (
+        <div>
+            <h1>Services page</h1>
+            <a href='/'>Home page</a>
+        </div>
+    )
+}
+
+export default ServicesFunc;
